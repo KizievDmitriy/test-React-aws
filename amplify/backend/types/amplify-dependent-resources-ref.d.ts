91554c2fd0c476c3b29655fe1918446ec2269d2a
KizievDmitriy <kizievdmitrii@gmail.com>
2023-06-27 12:09:45 +0300

    init amplifi cli and add auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "testreactawsb7dd593a": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Amplife from 'aws-amplify'
+import awsconfig from './aws-exports'
+import {AmplifySigneOute, withAuthenticator} from '@aws-amplify/ui-react'
+
+Amplife.configure(awsconfig)
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <AmplifySigneOute/>
+        <h2>My App Content</h2>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
